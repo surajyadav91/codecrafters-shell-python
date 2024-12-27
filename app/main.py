fc4d6d9b2c1f8d6c063728849471bbbf2e61ff97
surajyadav91 <surajyadav91@gmail.com>
2024-12-28 02:47:42 +0530

    REPL

--- app/main.py
+++ app/main.py
@@ -3,11 +3,20 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
+    valid_commands = []
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
 
-    # Wait for user input
-    command = input()
-    print(f'{command}: command not found')
+        command = input()
+
+        if command not in valid_commands:
+            print(f'{command}: command not found')
+            continue
+
+    # # Wait for user input
+    # command = input()
+    # print(f'{command}: command not found')
 
 
 if __name__ == "__main__":
